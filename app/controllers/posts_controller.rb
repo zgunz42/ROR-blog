64c036880db7aa53ed042077fc624e884e6394e4
adi gunawan <adi_gunawan@live.com>
2016-04-14 08:19:07 +0800

    added Post CRUD abilities

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to @post, :notice => 'zgunz your post ' << @post.title << ' created !'
+    else
+      render 'new', notice: 'zgunz sorry, something wrong :( !!'
+    end
+  end
+
+  def edit
+    # code here
+  end
+
+  def update
+    # code here
+    if @post.update post_params
+      redirect_to @post, notice: 'succesfully updated!'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    # code here
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def find_post
+    @post = Post.find(params[:id])
+  end
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
