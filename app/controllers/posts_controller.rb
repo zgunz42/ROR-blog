1695b664dbef1b98b3093a6944d199bd02c70c86
adi gunawan <adi_gunawan@live.com>
2016-04-20 14:24:20 +0800

    add frendly_id to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,9 +42,9 @@ class PostsController < ApplicationController
 
   private
   def find_post
-    @post = Post.find(params[:id])
+    @post = Post.friendly.find(params[:id])
   end
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :slug)
   end
 end
